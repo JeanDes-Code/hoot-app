3ff3b3150115213ab6ddaa5593564d6ae24d2a87
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-01-06 23:09:20 +0100

    Navbar partielle + install Redux

---
+++ serv_hoot/client/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { UidContext } from './AppContext';
+import Logout from './Log/Logout';
+
+const Navbar = () => {
+    const uid = useContext(UidContext);
+
+    return (
+        <nav>
+            <div className="nav-container">
+                <div className="logo">
+                    <NavLink to="/">
+                        <div className="logo">
+                            <img src="./img/icon.png" alt="logo" />
+                            <h3> Hoot</h3>
+                        </div>
+                    </NavLink>
+                </div>
+                {uid ? (
+                    <ul>
+                        <li></li>
+                        <li className="welcome">
+                            <NavLink to="/profil">
+                                <h5> Bienvenue Papa </h5>
+                            </NavLink>
+                        </li>
+                        <Logout />
+                    </ul>
+                ) : (
+                    <ul>
+                        <li></li>
+                        <li className="welcome">
+                            <NavLink to="/profil">
+                                <img src="./img/icons/login.svg" alt="login" />
+                            </NavLink>
+                        </li>
+                    </ul>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- serv_hoot/client/src/components/Routes/index.js
+++ serv_hoot/client/src/components/Routes/index.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Profil from '../../pages/Profil';
 import Trending from '../../pages/Trending';
+import Navbar from '../Navbar';
 
 const index = () => {
     return (
         <Router>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/profil" element={<Profil />} />
