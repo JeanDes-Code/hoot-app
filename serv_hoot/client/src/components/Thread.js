7e2d871f27e3cbf55106ebe5310b2a879fb8aaa8
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-01-10 15:10:53 +0100

    infinite scroll

--- serv_hoot/client/src/components/Thread.js
+++ serv_hoot/client/src/components/Thread.js
@@ -6,16 +6,29 @@ import Card from './Post/Card';
 
 const Thread = () => {
     const [loadPost, setLoadPost] = useState(true);
+    const [count, setCount] = useState(5);
     const dispatch = useDispatch();
 
     // @ts-ignore
     const posts = useSelector((state) => state.postReducer);
 
+    const loadMore = () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop + 1 >
+            document.scrollingElement.scrollHeight
+        ) {
+            setLoadPost(true);
+        }
+    };
+
     useEffect(() => {
         if (loadPost) {
-            dispatch(getPosts());
+            dispatch(getPosts(count));
             setLoadPost(false);
+            setCount(count + count);
         }
+        window.addEventListener('scroll', loadMore);
+        return () => window.removeEventListener('scroll', loadMore);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [loadPost]);
 
