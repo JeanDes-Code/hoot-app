c709e9ef45c9a215df89ca4452dc9425110450ec
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-01-12 14:30:14 +0100

    Trends page et "hot topics"

--- serv_hoot/client/src/actions/post.actions.js
+++ serv_hoot/client/src/actions/post.actions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // posts
 
 export const GET_POSTS = 'GET_POSTS';
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
 export const ADD_POST = 'ADD_POST';
 export const LIKE_POST = 'LIKE_POST';
 export const UNLIKE_POST = 'UNLIKE_POST';
@@ -10,10 +11,12 @@ export const UPDATE_POST = 'UPDATE_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 // comments
-
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
+
+//trends
+export const GET_TRENDS = 'GET_TRENDS';
 
 //errors
 export const GET_POST_ERRORS = 'GET_POST_ERRORS';
@@ -25,6 +28,7 @@ export const getPosts = (num) => {
             .then((res) => {
                 const array = res.data.slice(0, num);
                 dispatch({ type: GET_POSTS, payload: array });
+                dispatch({ type: GET_ALL_POSTS, payload: res.data });
             })
             .catch((err) => {
                 console.log(err);
@@ -167,5 +171,11 @@ export const deleteComment = (postId, commentId) => {
             .catch((err) => {
                 console.log(err);
             });
+    };
+};
+
+export const getTrends = (sortedArray) => {
+    return (dispatch) => {
+        dispatch({ type: GET_TRENDS, payload: sortedArray });
     };
 };
--- serv_hoot/client/src/pages/Trending.js
+++ serv_hoot/client/src/pages/Trending.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import { UidContext } from 'components/AppContext';
+import LeftNav from 'components/LeftNav';
+import Trends from 'components/Trends';
+import { isEmpty } from 'components/Utils';
+import React, { useContext } from 'react';
+import { useSelector } from 'react-redux';
+import Card from './../components/Post/Card';
 
 const Trending = () => {
-    return <div>Trends</div>;
+    const uid = useContext(UidContext);
+    // @ts-ignore
+    const trendList = useSelector((state) => state.trendingReducer);
+
+    return (
+        <div className="trending-page">
+            <LeftNav />
+            <div className="main">
+                <ul>
+                    {!isEmpty(trendList[0]) &&
+                        trendList.map((post) => (
+                            <Card post={post} key={post._id} />
+                        ))}
+                </ul>
+            </div>
+            <div className="right-side">
+                <div className="right-side-container">
+                    <Trends />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Trending;
